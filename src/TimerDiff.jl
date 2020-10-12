5abfd58f9b08fc4cf4444247d2defbc49a638edb
Harmen Stoppels <harmenstoppels@gmail.com>
2020-10-12 16:41:25 +0200

    allow comparison with arbitrary columns

--- src/TimerDiff.jl
+++ src/TimerDiff.jl
@@ -5,11 +5,28 @@ module TimerDiff
 import JSON
 import DataStructures: OrderedDict
 import Printf: @sprintf
-import StatsBase: mean, median
+import StatsBase: mean, median, median!, quantile!
 
 const KEEP = 0x00
 const DELETE = 0x01
 const ADD = 0x02
+
+const separator = " | "
+
+abstract type Node end
+
+struct TimingNode <: Node
+    identifier::String
+    timings::Vector{Float64}
+    subNodes::Vector{TimingNode}
+    totalTime::Float64
+
+    TimingNode(identifier::String, timings::Vector{Float64}, subNodes::Vector{TimingNode}) = new(identifier, timings, subNodes, sum(timings))
+end
+
+struct RootNode <: Node
+    subNodes::Vector{TimingNode}
+end
 
 """
 Get the edit distance between a and b
@@ -62,8 +79,7 @@ function backtrack(steps)
     while i != 1 || j != 1
         if steps[i, j] == KEEP
             push!(path, KEEP)
-            i -= 1
-            j -= 1
+            i -= 1; j -= 1
         elseif steps[i, j] == DELETE
             push!(path, DELETE)
             i -= 1
@@ -89,58 +105,6 @@ you need to add b[j] and increment j. Whenever you hit DELETE,
 you have to delete a[i] and increment i.
 """
 backtrack(a, b) = backtrack(first(diff_table(a, b)))
-
-function get_indent(done::Vector{Bool})
-    indent = ""
-
-    for idx = 1:length(done) - 1
-        indent *= done[idx] ? "   " : "│  "
-    end
-    
-    if !isempty(done)
-        indent *= done[end] ? "└─ " : "├─ "
-    end
-
-    return indent
-end
-
-struct Stat
-    count::Int
-    total::Float64
-    mean::Float64
-    median::Float64
-    min::Float64
-    max::Float64
-end
-
-struct StatDiff
-    count::Float64
-    total::Float64
-    mean::Float64
-    median::Float64
-    min::Float64
-    max::Float64
-end
-
-Stat(item::OrderedDict) = Stat(item["timings"])
-
-Stat(a::Vector) = Stat(
-    length(a),
-    sum(a),
-    mean(a),
-    median(a),
-    minimum(a),
-    maximum(a)
-)
-
-compare(a::Stat, b::Stat) = StatDiff(
-    b.count / a.count,
-    b.total / a.total,
-    b.mean / a.mean,
-    b.median / a.median,
-    b.min / a.min,
-    b.max / a.max
-)
 
 prettypercent(p) = string(@sprintf("%.2f", p * 100), "%")
 
@@ -178,18 +142,6 @@ function prettycount(b)
     return string(@sprintf("%.2f", value), " ", units)
 end
 
-function print_indented(indent::Vector{Bool}, key; io = stdout)
-    if length(indent) > 1
-        for is_done in @view(indent[2:end-1])
-            print(io, is_done ? "  " : "│ ")
-        end
-
-        print(io, indent[end] ? "└ " : "├ ")
-    end
-
-    print(io, rpad(key, 70 - 2 * length(indent)))
-end
-
 function describe_change(ratio; factor = 0.1)
     diff = ratio - 1
 
@@ -202,68 +154,145 @@ function describe_change(ratio; factor = 0.1)
     end
 end
 
-function default_on_keep(key, lhs, rhs, indent; io = stdout)
-    stat_lhs, stat_rhs = Stat(lhs), Stat(rhs)
-    ratio = compare(stat_lhs, stat_rhs)
+const diff_size = length("(+xxx.xx%)")
 
-    # If mean runtime has increased / decreased significantly, add a - or + respectively
-    # If number of calls changed significantly, add a !
-    runtime_change = describe_change(ratio.mean)
-    count_change = describe_change(ratio.count)
-    print(io, if runtime_change === :increased
-        "- "
-    elseif runtime_change === :decreased
-        "+ "
-    elseif count_change !== :no
-        "! "
-    else
-        "  "
-    end)
-    print_indented(indent, key, io = io)
-    print(io, "    ")
-    print(io, lpad(lpad(prettytime(stat_lhs.mean), 9) * " → " * lpad(prettytime(stat_rhs.mean), 9), 21), " ", lpad(prettydiff(ratio.mean), 12))
-    print(io, "    ")
-    print(io, lpad(lpad(prettycount(stat_lhs.count), 8) * " → " * lpad(prettycount(stat_rhs.count), 8), 21), " ", lpad(prettydiff(ratio.count), 12))
-    println(io)
+# column_size(x)
+
+struct StatFormat{diff}
+  stat::Symbol
+  header::String
+  space::Int
+
+  function StatFormat{diff}(s::Symbol) where diff
+    s === :count            && return new{diff}(s, "#", 9)
+    s === :total            && return new{diff}(s, "Total", 14)
+    s === :self             && return new{diff}(s, "Self", 14)
+    s === :mean             && return new{diff}(s, "Mean", 14)
+    s === :median           && return new{diff}(s, "Median", 14)
+    s === :quartilehigh     && return new{diff}(s, "Quartile High", 14)
+    s === :quartilelow      && return new{diff}(s, "Quartile Low", 14)
+    s === :min              && return new{diff}(s, "Min", 14)
+    s === :max              && return new{diff}(s, "Max", 14)
+    s === :percentage       && return new{diff}(s, "%", 11)
+    s === :parentpercentage && return new{diff}(s, "Parent %", 11)
+    s === :selfpercentage   && return new{diff}(s, "Self %", 11)
+    throw(ArgumentError("Unknown stat $s requested"))
+  end
 end
 
-function default_on_add_or_delete(key, item, mode, indent; io = stdout)
-    stat = Stat(item)
-    print(io, "  ")
-    print_indented(indent, key, io = io)
-    print(io, "    ")
-    print(io, rpad(lpad(prettytime(stat.mean), mode == DELETE ? 9 : 21), 34))
-    print(io, "    ")
-    print(io, rpad(lpad(prettycount(stat.count), mode == DELETE ? 10 : 21), 34))
-    println(io)
+function max_node_identifier_length(node::Node, recursionDepth = 0, addPerLevel = 2, len = 0)
+    if node isa TimingNode
+        currentLength = length(node.identifier) + recursionDepth * addPerLevel
+        len = max(currentLength, len)
+    end
+
+    for child in node.subNodes
+        len = max_node_identifier_length(child, recursionDepth + 1, addPerLevel, len)
+    end
+
+    return len
 end
 
-function just_walk(a::OrderedDict{String}, mode::UInt8, indent::Vector{Bool}, on_add_or_delete)
-    push!(indent, false)
-    for (idx, (key, val)) in enumerate(a)
-        if idx == length(a)
-            indent[end] = true
+struct StatWrapper
+    node::TimingNode
+    rootTime::Float64
+    parentTime::Float64
+    subTime::Float64
+end
+
+safe_ratio(a, b) = iszero(b) || a > b ? 1.0 : a / b
+safe_self(total, sub) = max(0.0, total - sub)
+
+compute(::Val{:count}, wrap::StatWrapper) = length(wrap.node.timings)
+compute(::Val{:total}, wrap::StatWrapper) = wrap.node.totalTime
+compute(::Val{:self}, wrap::StatWrapper) = safe_self(wrap.node.totalTime, wrap.subTime)
+compute(::Val{:mean}, wrap::StatWrapper) = wrap.node.totalTime / length(wrap.node.timings)
+compute(::Val{:median}, wrap::StatWrapper) = median!(wrap.node.timings)
+compute(::Val{:quartilehigh}, wrap::StatWrapper) = quantile!(wrap.node.timings, 0.75)
+compute(::Val{:quartilelow}, wrap::StatWrapper) = quantile!(wrap.node.timings, 0.25)
+compute(::Val{:min}, wrap::StatWrapper) = minimum(wrap.node.timings)
+compute(::Val{:max}, wrap::StatWrapper) = maximum(wrap.node.timings)
+compute(::Val{:percentage}, wrap::StatWrapper) = safe_ratio(wrap.node.totalTime, wrap.rootTime)
+compute(::Val{:parentpercentage}, wrap::StatWrapper) = safe_ratio(wrap.node.totalTime, wrap.parentTime)
+compute(::Val{:selfpercentage}, wrap::StatWrapper) = safe_ratio(safe_self(wrap.node.totalTime, wrap.subTime), wrap.node.totalTime)
+
+format(::Val{:count}, val) = prettycount(val)
+format(::Val{:total}, val) = prettytime(val)
+format(::Val{:self}, val) = prettytime(val)
+format(::Val{:mean}, val) = prettytime(val)
+format(::Val{:median}, val) = prettytime(val)
+format(::Val{:quartilehigh}, val) = prettytime(val)
+format(::Val{:quartilelow}, val) = prettytime(val)
+format(::Val{:min}, val) = prettytime(val)
+format(::Val{:max}, val) = prettytime(val)
+format(::Val{:percentage}, val) = prettypercent(val)
+format(::Val{:parentpercentage}, val) = prettypercent(val)
+format(::Val{:selfpercentage}, val) = prettypercent(val)
+
+abstract type Mode end
+struct Normal <: Mode end
+struct Delete <: Mode end
+struct Add <: Mode end;
+
+function print_stat(io, f, lhs::StatWrapper, rhs::StatWrapper)
+    from = compute(Val(f.stat), lhs)
+    to = compute(Val(f.stat), rhs)
+    diff = prettydiff(to / from)
+    result = lpad(lpad(format(Val(f.stat), from), f.space) * " → " * lpad(format(Val(f.stat), to), f.space) * " " * lpad(diff, diff_size), column_space(f))
+    print(io, result, separator)
+end
+
+function print_stat(io, f, x::StatWrapper, mode::Mode = Normal())
+    result = format(Val(f.stat), compute(Val(f.stat), x))
+    if mode isa Delete
+        result *= " " ^ (3 + f.space + 1 + diff_size)
+    elseif mode isa Add
+        result *= " " ^ (1 + diff_size)
+    end
+
+    print(io, lpad(result, column_space(f)), separator)
+end
+
+function print_node_diff(io, a::Node, b::Node, formats, identifierSpace, nodePrefix = "", isLastSubnode = false, parentTimeLhs = 0.0, parentTimeRhs = 0.0, rootTimeLhs = 0.0, rootTimeRhs = 0.0, depth = 0)
+    subTimeLhs = mapreduce(child -> child.totalTime, +, a.subNodes, init=0.0)
+    subTimeRhs = mapreduce(child -> child.totalTime, +, b.subNodes, init=0.0)
+
+    if a isa TimingNode && b isa TimingNode
+        stats_a = StatWrapper(a, rootTimeLhs, parentTimeLhs, subTimeLhs)
+        stats_b = StatWrapper(b, rootTimeRhs, parentTimeRhs, subTimeRhs)
+
+        total_runtime_change = describe_change(compute(Val(:total), stats_b) / compute(Val(:total), stats_a))
+        mean_runtime_change = describe_change(compute(Val(:mean), stats_b) / compute(Val(:mean), stats_a))
+        count_change = describe_change(compute(Val(:count), stats_b) / compute(Val(:count), stats_a))
+        
+        print(io, if total_runtime_change === :increased || mean_runtime_change === :increased
+            "- "
+        elseif total_runtime_change === :decreased || mean_runtime_change === :decreased
+            "+ "
+        elseif count_change !== :no
+            "! "
+        else
+            "  "
+        end)
+
+        identifier = rpad(nodePrefix * (depth > 1 ? (isLastSubnode ? "└ " : "├ ") : (depth <= 1 ? "" : "  ")) * a.identifier, identifierSpace)
+        print(io, identifier, separator)
+
+        for format in formats
+            print_stat(io, format, stats_a, stats_b)
         end
 
-        on_add_or_delete(key, val, mode, indent)
-        just_walk(val["sub-timings"], mode, indent, on_add_or_delete)
+        println(io)
+
+        parentTimeLhs, parentTimeRhs = a.totalTime, b.totalTime 
     end
-    pop!(indent)
-end
 
-function print_header(; io = stdout)
-    print(io,
-"""
-  ================================================================================================================================================
-                                                                                      Mean                                    #
-  ------------------------------------------------------------------------------------------------------------------------------------------------
-""")
-end
-
-function walk(a::OrderedDict{String}, b::Union{Nothing,OrderedDict{String}}, indent::Vector{Bool} = Bool[], on_keep = default_on_keep, on_add_or_delete = default_on_add_or_delete)
+    if depth > 1
+        nodePrefix *= isLastSubnode ? "  " : "│ "
+    end
     
     # ordered set of keys -- just create a copy for ease
-    keys_a, keys_b = collect(keys(a)), collect(keys(b))
+    keys_a, keys_b = [child.identifier for child in a.subNodes], [child.identifier for child in b.subNodes]
 
     # actions for diffing: keep / add / delete
     actions = backtrack(keys_a, keys_b)
@@ -271,46 +300,148 @@ function walk(a::OrderedDict{String}, b::Union{Nothing,OrderedDict{String}}, ind
     # indices for keys
     i, j = 1, 1
 
-    push!(indent, false)
-
     for (idx, action) in enumerate(actions)
-        if idx == length(actions)
-            indent[end] = true
+        is_last = idx == length(actions)
+
+        if a isa RootNode && (action == KEEP || action == DELETE)
+            rootTimeLhs = a.subNodes[i].totalTime
         end
 
+        if b isa RootNode && (action == KEEP || action == ADD)
+            rootTimeRhs = b.subNodes[j].totalTime
+        end
+        
         if action == KEEP
-            key = keys_a[i]; lhs, rhs = a[key], b[key]
-            on_keep(key, lhs, rhs, indent)
-            walk(lhs["sub-timings"], rhs["sub-timings"], indent, on_keep, on_add_or_delete)
+            print_node_diff(io, a.subNodes[i], b.subNodes[j], formats, identifierSpace, nodePrefix, is_last, parentTimeLhs, parentTimeRhs, rootTimeLhs, rootTimeRhs, depth + 1)
             i += 1; j += 1
         elseif action == DELETE
-            key = keys_a[i]; lhs = a[key]
-            on_add_or_delete(key, lhs, DELETE, indent)
-            just_walk(lhs["sub-timings"], DELETE, indent, on_add_or_delete)
+            print_node(io, a.subNodes[i], formats, identifierSpace, nodePrefix, is_last, parentTimeLhs, rootTimeLhs, depth + 1, Delete())
             i += 1
         else
-            key = keys_b[j]; rhs = b[key]
-            on_add_or_delete(key, rhs, ADD, indent)
-            just_walk(rhs["sub-timings"], ADD, indent, on_add_or_delete)
+            print_node(io, b.subNodes[j], formats, identifierSpace, nodePrefix, is_last, parentTimeRhs, rootTimeRhs, depth + 1, Add())
             j += 1
         end
     end
 
-    pop!(indent)
+    return nothing
+end
+
+function print_node(io, x::Node, formats, identifierSpace, nodePrefix = "", isLastSubnode = false, parentTime = 0.0, rootTime = 0.0, depth = 0, mode::Mode = Normal())
+    subTime = mapreduce(child -> child.totalTime, +, x.subNodes, init=0.0)
+
+    if x isa TimingNode
+        if mode isa Add || mode isa Delete
+            print(io, "  ")
+        end
+
+        identifier = rpad(nodePrefix * (depth > 1 ? (isLastSubnode ? "└ " : "├ ") : (depth <= 1 ? "" : "  ")) * x.identifier, identifierSpace)
+        print(io, identifier, separator)
+
+        for format in formats
+            print_stat(io, format, StatWrapper(x, rootTime, parentTime, subTime), mode)
+        end
+
+        println(io)
+
+        parentTime = x.totalTime
+    end
+
+    if depth > 1
+        nodePrefix *= isLastSubnode ? "  " : "│ "
+    end
+
+    for (idx, child) in enumerate(x.subNodes)
+        if x isa RootNode
+            rootTime = child.totalTime
+        end
+        print_node(io, child, formats, identifierSpace, nodePrefix, idx == length(x.subNodes), parentTime, rootTime, depth + 1, mode)
+    end
+    
+    return nothing
+end
+
+# just printing the value
+column_space(x::StatFormat{false}) = x.space
+
+# printing a diff requires 2 times normal space + space for diff percentage.
+column_space(x::StatFormat{true}) = 2 * x.space + 4 + diff_size
+
+function print_things(lhs::RootNode, rhs::RootNode, statistics::Vector{Symbol}; io = stdout)
+    # calculate space for printing identifiers
+    identifierSpace = max(max_node_identifier_length(lhs), max_node_identifier_length(rhs))
+    totalSpace = identifierSpace
+
+    formats = [StatFormat{true}(stat) for stat in statistics]
+    totalSpace += mapreduce(column_space, +, formats, init = 0)
+    totalSpace += length(separator) * (length(formats) + 1) - 1
+
+    # Table start
+    println(io, "  ", '=' ^ totalSpace)
+
+    # Header
+    print(io, "  ", " " ^ identifierSpace, separator)
+    for format in formats
+        print(io, lpad(format.header, column_space(format)), separator)
+    end
+    println(io)
+
+    # Header separation line
+    println(io, "  ", '-' ^ totalSpace)
+
+    print_node_diff(io, lhs, rhs, formats, identifierSpace)
+
+    # End table
+    println(io, "  ", '=' ^ totalSpace)
 
     return nothing
 end
 
-to_json(a::String) = JSON.parsefile(a, dicttype=OrderedDict)
+function print_things(x::RootNode, statistics::Vector{Symbol}; io = stdout)
+    # calculate space for printing identifiers
+    identifierSpace = max_node_identifier_length(x)
+    totalSpace = identifierSpace
 
-to_table(a::String, b::String; io = stdout) = to_table(to_json(a), to_json(b), io = io)
+    formats = [StatFormat{false}(stat) for stat in statistics]
+    totalSpace += mapreduce(column_space, +, formats, init = 0)
+    totalSpace += length(separator) * (length(formats) + 1) - 1
 
-function to_table(a, b; io = stdout)
-    print_header(io = io)
-    on_keep = (key, lhs, rhs, indent) -> default_on_keep(key, lhs, rhs, indent, io = io)
-    on_add_or_delete = (key, item, mode, indent) -> default_on_add_or_delete(key, item, mode, indent, io = io)
-    walk(a, b, Bool[], on_keep, on_add_or_delete)
+    # Table start
+    println(io, '=' ^ totalSpace)
+
+    # Header
+    print(io, " " ^ identifierSpace, separator)
+    for format in formats
+        print(io, lpad(format.header, column_space(format)), separator)
+    end
+    println(io)
+
+    # Header separtion line
+    println(io, '-'^totalSpace)
+
+    print_node(io, x, formats, identifierSpace)
+
+    # End table
+    println(io, '='^totalSpace)
+
+    return nothing
 end
 
+function parse_json(x::OrderedDict)
+    children = Vector{TimingNode}()
+    for (key, value) in x
+        push!(children, TimingNode(
+            key,
+            convert(Vector{Float64}, value["timings"]),
+            parse_json(value["sub-timings"])
+        ))
+    end
+    return children
+end
+
+
+to_json(a::String) = RootNode(parse_json(JSON.parsefile(a, dicttype=OrderedDict)))
+
+to_table(a::String, b::String; stats = [:mean, :count, :total], io = stdout) = print_things(to_json(a), to_json(b), stats, io = io)
+to_table(a::String; stats = [:mean, :count, :total], io = stdout) = print_things(to_json(a), stats, io = io)
 
 end # module
